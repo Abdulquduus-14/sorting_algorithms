eacc1d2ce5e828f80796c2b5f7f3b2b6fb924e1c
Amidu Qudus <qudusamidu14@gmail.com>
2023-04-24 18:09:47 -0700

    tsk3

---
+++ 3-quick_sort.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+/**
+ * quick_sort -  a function that sorts an array of integers in
+ * ascending order using the Quick sort algorithm
+ *
+ * @array: array of elements to sort
+ * @size: number of elements in array
+ *
+ * Return: void
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	qs(array, 0, size - 1);
+}
+
+/**
+ * qs - main quicksort function
+ * @items: array of elements to sort
+ * @left: leftmost element
+ * @right: rightmost element
+ *
+ * return: nothing
+ */
+void qs(int *items, size_t left, size_t right)
+{
+	size_t i, j;
+	int x, y;
+
+	i = left;
+	j = right;
+	x = items[(left + right) / 2];
+	do {
+		while ((items[i] < x) && (i < right))
+			i++;
+		while ((x < items[j]) && (j > left))
+			j--;
+		if (i <= j)
+		{
+			y = items[i];
+			items[i] = items[j];
+			items[j] = y;
+			i++;
+			j--;
+		}
+	} while (i <= j);
+
+	if (left < j)
+		qs(items, left, j);
+	if (i < right)
+		 qs(items, i, right);
+}
